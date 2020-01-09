5e2a3b5322c02d10a051d3f1a5117a06bbd0f12d
musashi0128 <k.miyamoto.0128@gmail.com>
2020-01-09 20:05:43 +0900

    chartの表示をapiから取得して表示させる

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,11 +1,30 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
+// チャートのキャンドルスティックの構造体
+// 配列のような挙動をする参照型 -> スライスは動的である
+// ここでは送られてくるCandleを随時保管する
 type DataFrameCandle struct {
 	ProductCode string        `json:"product_code"`
 	Duration    time.Duration `json:"duration"`
 	Candles     []Candle      `json:"candles"`
+	Smas        []Sma         `json:"smas,omitempty"`
+	Emas        []Ema         `json:"emas,omitempty"`
+}
+
+// チャートの単純移動平均線の構造体
+type Sma struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
+}
+
+// チャートの指数平滑移動平均線の構造体
+type Ema struct {
+	Period int       `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -55,3 +74,28 @@ func (df *DataFrameCandle) Volume() []float64 {
 	}
 	return s
 }
+
+// DataFrameCandleにデータを追加する
+// smaのデータをtalib.Smaで算出して追加していく
+// func (df *DataFrameCandle) AddSma(period int) bool {
+// 	if len(df.Candles) > period {
+// 		df.Smas = append(df.Smas, Sma{
+// 			Period: period,
+// 			Values: talib.Sma(df.Closes(), period),
+// 		})
+// 		return true
+// 	}
+// 	return false
+// }
+
+// // smaのデータをtalib.Emaで算出して追加していく
+// func (df *DataFrameCandle) AddEma(period int) bool {
+// 	if len(df.Candles) > period {
+// 		df.Emas = append(df.Emas, Ema{
+// 			Period: period,
+// 			Values: talib.Ema(df.Closes(), period),
+// 		})
+// 		return true
+// 	}
+// 	return false
+// }
